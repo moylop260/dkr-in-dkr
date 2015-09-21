9ef728c225cb8c0cc5839595e11b644158167681
Moises Lopez <moylop260@vauxoo.com>
2015-09-20 23:34:31 -0400

    squash

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ fi
 if [ -z "${MYPORT}" ]
     then export MYPORT="12376"
 fi
-if [ -z "${CONTAINER_NAME}" ]
-    then export CONTAINER_NAME="$IMAGE"-test
+if [ -z "${NAME}" ]
+    then export NAME="$IMAGE"-test
 fi
 
 if [ -z "${CONTAINER_VOLUME}" ]
     then export CONTAINER_VOLUME="${HOME}/$IMAGE"
 fi
 
-export CONTAINER_VOLUME_PATH="$CONTAINER_VOLUME"/"$CONTAINER_NAME"
+export CONTAINER_VOLUME_PATH="$CONTAINER_VOLUME"/"$NAME"
 
 docker build $1 -t $IMAGE .
-docker run --privileged -v $CONTAINER_VOLUME:/var/lib/docker -d -p $MYPORT:2376 --name=$CONTAINER_NAME $IMAGE $2
+docker run --privileged -v $CONTAINER_VOLUME:/var/lib/docker -d -p $MYPORT:2376 --name=$NAME $IMAGE $2
 
