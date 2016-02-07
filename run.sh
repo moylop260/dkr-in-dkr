7e25908d26c1183be59029121d7514d015ef75e8
Moises Lopez <moylop260@vauxoo.com>
2016-02-07 15:59:01 -0500

    [REF] run.sh: Add PORTS and device reads bps

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@
 set -v
 
 if [ -z "${IMAGE}" ]
-    then export IMAGE="dkr-in-dkr"
+    then export IMAGE="dkr-in-dkr:201602"
 fi
 if [ -z "${PORT}" ]
     then export PORT="12376"
 fi
+
+if [ -z "${PORTS}" ]
+    then export PORTS="2000-2099"
+fi
+
 if [ -z "${NAME}" ]
     then export NAME="$IMAGE"-test
 fi
@@ -19,5 +24,5 @@ fi
 export CONTAINER_VOLUME_PATH="$CONTAINER_VOLUME"/"$NAME"
 
 docker build $1 -t $IMAGE .
-docker run --privileged -m 8g --device-write-bps=/dev/sda:10mb --device-read-bps=/dev/sda:10mb --device-write-bps=/dev/md2:10mb --device-read-bps=/dev/md2:10mb --device-write-bps=/dev/sdb:10mb --device-read-bps=/dev/sdb:10mb -v $CONTAINER_VOLUME_PATH:/var/lib/docker -d -p $PORT:2376 --name=$NAME $IMAGE $2
+docker run --privileged -m 8g --device-write-bps=/dev/sda:10mb --device-read-bps=/dev/sda:10mb --device-write-bps=/dev/md2:10mb --device-read-bps=/dev/md2:10mb --device-write-bps=/dev/sdb:10mb --device-read-bps=/dev/sdb:10mb -v $CONTAINER_VOLUME_PATH:/var/lib/docker -d -p $PORT:2376 -p ${PORTS}:${PORTS} --name=$NAME $IMAGE $2
 
